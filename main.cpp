6c1361dcdf11a64ec50087173532b226f6853fdf
Pete Goodwin <github@imekon.org>
2019-03-02 17:03:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,231 @@
+// ImGui - standalone example application for Glfw + OpenGL 3, using programmable pipeline
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <windows.h>
+#include <iostream>
+#include <imgui.h>
+#include "imgui_impl_glfw_gl3.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+GLuint VAO, VBO, shader, uniform_model;
+
+bool direction = true;
+float triangle_offset = 0.0f;
+float triangle_max_offset = 0.7f;
+float triangle_increment = 0.005f;
+
+float current_angle = 0.0f;
+
+const float toRadians = (float)M_PI / 180.0f;
+
+const char *vShader = "														\n\
+#version 330																\n\
+layout (location = 0) in vec3 pos;											\n\
+uniform mat4 model;															\n\
+void main()																	\n\
+{																			\n\
+	gl_Position = model * vec4(pos.x * 0.4, pos.y * 0.4, pos.z, 1.0);		\n\
+}";
+
+const char *fShader = "														\n\
+#version 330																\n\
+out vec4 colour;															\n\
+void main()																	\n\
+{																			\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);										\n\
+}";
+
+static void create_triangle()
+{
+	GLfloat vertices[] =
+	{
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void add_shader(GLuint program, const char *shader_code, GLenum shader_type)
+{
+	auto shader = glCreateShader(shader_type);
+	const GLchar *theCode[1];
+	theCode[0] = shader_code;
+
+	GLint codeLength[1];
+	codeLength[0] = (GLint)strlen(shader_code);
+
+	glShaderSource(shader, 1, theCode, codeLength);
+	glCompileShader(shader);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+	glAttachShader(program, shader);
+}
+
+void compile_shaders()
+{
+	shader = glCreateProgram();
+	if (!shader)
+	{
+		cout << "Failed to create shader" << endl;
+		return;
+	}
+
+	add_shader(shader, vShader, GL_VERTEX_SHADER);
+	add_shader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+
+	uniform_model = glGetUniformLocation(shader, "model");
+}
+
+static void error_callback(int error, const char* description)
+{
+	cout << "Error " << description << endl;
+}
+
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    // Setup window
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+	// Setup GLEW
+	if (glewInit() != GLEW_OK)
+		return -1;
+
+    // Setup ImGui binding
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+    // Load Fonts
+    // (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+    //ImGuiIO& io = ImGui::GetIO();
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+
+	create_triangle();
+	compile_shaders();
+
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(32, 32, 32);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+		if (direction)
+			triangle_offset += triangle_increment;
+		else
+			triangle_offset -= triangle_increment;
+
+		if (abs(triangle_offset) >= triangle_max_offset)
+			direction = !direction;
+
+		current_angle += 1.0f;
+		if (current_angle > 360.0f)
+			current_angle = 0.0f;
+
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+
+        // Rendering
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+
+        glViewport(0, 0, width, height);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triangle_offset, 0.0f, 0.0f));
+		model = glm::rotate(model, current_angle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(uniform_model, 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+		glUseProgram(0);
+
+        ImGui::Render();
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplGlfwGL3_Shutdown();
+    glfwTerminate();
+
+    return 0;
+}
